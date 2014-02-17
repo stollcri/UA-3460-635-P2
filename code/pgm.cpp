f2bcf462a1d07fc7ae83a1e7e4d3cb86d1119c8f
michaelcrouse <mec9@zips.uakron.edu>
2014-02-16 23:03:13 -0500

    Read in data from a pgm file (ASCII)

--- code/pgm.cpp
+++ code/pgm.cpp
@@ -3,13 +3,57 @@
 	Created by Michael Crouse, 2/16/14
 */
 #include "pgm.h"
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
 
 /* pgm constructor
 	creates a pgm objecct from a pgm file, either binary or ASCII
 	fileType is expecting pgmBinary or pgmASCII, which are defined in the header
 */
+//TODO: break two types into helper private methods
 pgm::pgm(const char * filename, int fileType) {
-	
+	ifstream inFile(filename);
+	string in;
+	char item[MAX_ITEM_LENGTH];
+	int tempValue;
+	if (fileType == pgmASCII) {
+		//TODO: check to make sure file is well-formed and opened properly
+		getline(inFile, in, '\n'); //ignore first "P2"
+		getline(inFile, in, '\n'); //get second line
+		while (in[0] == '#') getline(inFile, in); //get any further comments
+		//we should now have a line with two ints..
+		readTwoShorts(in, &width, &height);
+		getline(inFile, in, '\n'); //get the depth line
+		readOneChar(in, &depth);
+		//we have our x, y and z, so now we know the size of the matrix we must make
+		imageMatrix = new unsigned char *[height]; //make Y rows
+		for (int i = 0; i < height; i++) imageMatrix[i] = new unsigned char[width];
+		cout << "Image matrix: \n";
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				inFile >> item;
+				tempValue = atoi(item);
+				imageMatrix[i][j] = (unsigned char) tempValue;
+				cout << (int) imageMatrix[i][j] << ' ';
+			}
+			cout << "\n";
+		}
+		inFile.close();
+	}
+	else if (fileType == pgmBinary) {
+	}
+	else {
+		cerr << "Invalid file type provided to pgm class\n";
+		width = height = 0;
+		depth = (unsigned char) 0;
+	}
+	cout << "height " << height << " width " << width << " depth " << (int) depth << "\n";
 }
 
 /* toBinary outputs the object to a binary .pgm file
@@ -30,4 +74,44 @@ int pgm::toASCII(const char * filename) {
 //destructor - clears up the image matrix safely
 pgm::~pgm() {
 	
+}
+
+/* Get two space seperated numbers from the begining of a string of characters
+	Borrowed from P1 to simplify implementation
+*/
+void readTwoShorts(string line, unsigned short *x, unsigned short *y) {
+	unsigned short tmpX = 0, tmpY = 0;
+	unsigned short nextDigit = 0, currentNumber = 0;
+	int loopCount = 0;
+	while (line[loopCount]) {
+		if (isspace(line[loopCount]) || line[loopCount] == '\n') {
+			tmpX = currentNumber;
+			*x = tmpX;
+			currentNumber = 0;
+		} else {
+			nextDigit = line[loopCount] - '0';
+			currentNumber *= 10;
+			currentNumber += nextDigit;
+		}
+		++loopCount;
+	}
+	tmpY = currentNumber;
+	*y = tmpY;
+}
+
+/* Get a single short from a line
+	Borrowed from P1 to simplify implementation
+*/
+void readOneChar(string line, unsigned char * z) {
+	unsigned char tmpZ = (unsigned char) 0;
+	int nextDigit = 0, currentNumber = 0;
+	int loopCount = 0;
+	while (line[loopCount]) {
+		nextDigit = line[loopCount] - '0';
+		currentNumber *= 10;
+		currentNumber += nextDigit;
+		++loopCount;
+	}
+	tmpZ = (unsigned char) currentNumber;
+	*z = tmpZ;
 }
--- code/pgm.h
+++ code/pgm.h
@@ -4,13 +4,17 @@
 	NOTE: this implementation assumes an 8 bit greyscale depth per
 	the program requirements listed in the pdf
 */
-
 #ifndef PGM_H
 #define PGM_H
+#include <string>
 //pgmBinary and pgmASCII are definitions for use with the constructor, denoting
 //the file type from which we are creating the object
 #define pgmBinary 0
 #define pgmASCII 1
+
+#define MAX_ITEM_LENGTH 80
+
+using namespace std;
 
 class pgm {
 private:
@@ -24,5 +28,9 @@ public:
 	int toASCII(const char * filename);
 	~pgm();
 };
+
+//helper functions
+void readTwoShorts(string line, unsigned short *x, unsigned short *y);
+void readOneChar(string line, unsigned char * z);
 
 #endif
